ae2674bca0bdf162d828b26ff645022b9903442a
arturgontijo <arturgontijo@gmail.com>
2018-11-30 09:39:48 -0200

    [Repo] Fix README input keys

--- finance/cntk-next-day-trend/test_next_day_trend_service.py
+++ finance/cntk-next-day-trend/test_next_day_trend_service.py
@@ -34,13 +34,13 @@ if __name__ == "__main__":
         if contract == "":
             contract = "SPY"
 
-        start_date = input("Start Date(2000-01-01): ") if not test_flag else ""
-        if start_date == "":
-            start_date = "2018-01-01"
+        start = input("Start Date(2000-01-01): ") if not test_flag else ""
+        if start == "":
+            start = "2018-01-01"
 
-        end_date = input("End Date(2009-01-01): ") if not test_flag else ""
-        if end_date == "":
-            end_date = "2018-11-01"
+        end = input("End Date(2009-01-01): ") if not test_flag else ""
+        if end == "":
+            end = "2018-11-01"
 
         target_date = input("Target Date(2018-11-12): ") if not test_flag else ""
         if target_date == "":
@@ -50,8 +50,8 @@ if __name__ == "__main__":
             stub = grpc_bt_grpc.NextDayTrendStub(channel)
             grpc_input = grpc_bt_pb2.Input(source=source,
                                            contract=contract,
-                                           start=start_date,
-                                           end=end_date,
+                                           start=start,
+                                           end=end,
                                            target_date=target_date)
             grpc_output = stub.trend(grpc_input)
             print(grpc_output.response)
