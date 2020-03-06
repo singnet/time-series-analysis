0d3778377bed17230b7d79ae58dac40f09c32362
arturgontijo <arturgontijo@gmail.com>
2020-03-06 13:01:35 -0300

    [orb] Using the right DOCKER_CONTAINER name.

--- .circleci/check_md5.sh
+++ .circleci/check_md5.sh
@@ -14,7 +14,7 @@ cd "$SERVICE_DIR" || exit
 for TARGET_FILE in "${ARR[@]}"
 do
   CURRENT_MD5=$(md5sum "${TARGET_FILE}")
-  REMOTE_MD5=$(ssh -o "StrictHostKeyChecking no" "${SSH_USER}"@"${SSH_HOST}" docker exec "${PROD_TAG}""${DOCKER_CONTAINER_NAME}" md5sum "${TARGET_FILE}")
+  REMOTE_MD5=$(ssh -o "StrictHostKeyChecking no" "${SSH_USER}"@"${SSH_HOST}" docker exec "${PROD_TAG}""${DOCKER_CONTAINER}" md5sum "${TARGET_FILE}")
 
   # Getting only the hash
   CURRENT_MD5=$(echo "${CURRENT_MD5}" | awk '{ print $1 }')
